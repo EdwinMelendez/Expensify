491a4278d2d5eef650b4ef208e81777dea9cf6e6
DarthVader <edwin.melendez@students.ipr.edu>
2018-05-08 10:32:01 -0500

    reverted accidental changes

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const EditExpensePage = (props) => {
-  return (
-      <div>
-          This is from my edit expense component
-      </div>
-  );
-};
+const EditExpensePage = () => (
+  <div>
+    This is from my edit expense component
+  </div>
+);
 
 export default EditExpensePage;
